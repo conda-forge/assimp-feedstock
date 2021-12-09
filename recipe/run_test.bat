d7851b94dbe6992f1c054fac6e0ebd84ff15936d
Silvio Traversaro <silvio@traversaro.it>
2021-12-09 09:28:48 +0100

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,12 +3,12 @@ setlocal EnableDelayedExpansion
 cd test
 
 :: Compile example that links assimp
-cmake -DCMAKE_BUILD_TYPE=Release .
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
 if errorlevel 1 exit 1
 
 :: Run example
-.\Release\assimp_main.exe
+.\assimp_main.exe
 if errorlevel 1 exit 1
